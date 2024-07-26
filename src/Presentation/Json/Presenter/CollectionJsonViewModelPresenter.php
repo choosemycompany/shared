3a1fd6074739a73c2f4c829930512393f087be11
Mathias Duchossoy <mathias.duchossoy@choosemycompany.com>
2024-07-26 12:40:28 +0200

    fix collection json presenter

--- src/Presentation/Json/Presenter/CollectionJsonViewModelPresenter.php
+++ src/Presentation/Json/Presenter/CollectionJsonViewModelPresenter.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace ChooseMyCompany\Shared\Presentation\Json\Presenter;
 
-use ChooseMyCompany\Shared\Domain\ValueObject\Pagination\PaginationDetails;
 use ChooseMyCompany\Shared\Presentation\Json\ViewModel\CollectionJsonViewModel;
-use ChooseMyCompany\Shared\Presentation\Shared\Pagination\PaginationViewModelBuilderTrait;
 
 class CollectionJsonViewModelPresenter extends JsonViewModelPresenter
 {
-    protected function initializeViewModel(array $items, PaginationDetails $paginationDetails): void
+    protected function initializeViewModel(array $items): void
     {
         $this->viewModel = new CollectionJsonViewModel(
             data: $items
